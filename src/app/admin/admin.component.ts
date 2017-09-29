95d65a04dd1adebc77ac9cac87987e4e00b405fc
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-09-29 09:38:20 -0700

    add functionality to components.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -5,13 +5,19 @@ import { Player } from '../player.model';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [PlayerService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
+  }
+
+  submitForm(name: string, position: string, description: string) {
+    let newPlayer: Player = new Player(name, position, description);
+    this.playerService.addPlayer(newPlayer);
   }
 
 }
